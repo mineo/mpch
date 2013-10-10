c17ab38ac594fec13782008921667395363250b2
Wieland Hoffmann <themineo@gmail.com>
2013-10-10 17:11:03 +0200

    initial commit

---
+++ mpd.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Network.MPD as MPD
+
+import Data.Text()
+
+addr :: Maybe String
+addr = Just "q150"
+port :: Maybe a
+port = Nothing
+mpd :: MPD.MPD a -> IO (MPD.Response a)
+mpd = MPD.withMPD_ addr port
+
+getMBIDfromResponse :: MPD.Response (Maybe MPD.Song) -> Maybe [MPD.Value]
+getMBIDfromResponse (Right (Just song)) = MPD.sgGetTag MPD.MUSICBRAINZ_TRACKID song
+getMBIDfromResponse _ = Nothing
+
+main :: IO ()
+main = mpd MPD.currentSong >>= \resp -> print $ getMBIDfromResponse resp
