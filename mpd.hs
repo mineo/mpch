e109f0306dca15660527d7f82c764ccfe0378770
Wieland Hoffmann <themineo@gmail.com>
2014-03-07 16:34:42 +0100

    error message

--- mpd.hs
+++ mpd.hs
@@ -11,12 +11,16 @@ port = Nothing
 mpd :: MPD.MPD a -> IO (MPD.Response a)
 mpd = MPD.withMPD_ addr port
 
-getMBIDfromResponse :: MPD.Response (Maybe MPD.Song) -> Maybe [MPD.Value]
-getMBIDfromResponse (Right (Just song)) = MPD.sgGetTag MPD.MUSICBRAINZ_TRACKID song
+handleResponse :: MPD.Response (Maybe MPD.Song) -> IO ()
+handleResponse (Left e) = print e
+handleResponse (Right content) = print $ getMBIDfromResponse content
+
+getMBIDfromResponse :: Maybe MPD.Song -> Maybe [MPD.Value]
+getMBIDfromResponse (Just song) = MPD.sgGetTag MPD.MUSICBRAINZ_TRACKID song
 getMBIDfromResponse _ = Nothing
 
 main :: IO ()
 {-main = mpd MPD.currentSong >>= \resp -> print $ getMBIDfromResponse resp-}
 main = do
        resp <- mpd MPD.currentSong
-       print $ getMBIDfromResponse resp 
+       handleResponse resp 
