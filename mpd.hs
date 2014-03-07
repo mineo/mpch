e9e516102cfe43bfc8cc4e0d393455bcff4bd846
Wieland Hoffmann <themineo@gmail.com>
2014-03-07 23:40:53 +0100

    some niceties

--- mpd.hs
+++ mpd.hs
@@ -2,7 +2,6 @@
 
 import qualified Network.MPD as MPD
 
-import Data.Maybe (fromMaybe)
 import Data.Text ()
 import System.Console.GetOpt (OptDescr(Option), ArgDescr(OptArg), getOpt, ArgOrder(Permute))
 import System.Environment (getArgs)
@@ -37,14 +36,14 @@ mpd action config = MPD.withMPD_ h p action
     where h = host config
           p = port config
 
-parseArgs :: [String] -> ([(Config -> Config)], [String], [String])
-parseArgs argv = getOpt Permute options argv
+parseArgs :: [String] -> ([Config -> Config], [String], [String])
+parseArgs = getOpt Permute options
 
 handleArgs :: (b, t, [[a]]) -> Either [a] b
 handleArgs opts = case opts of
-                 (args, _, []) -> do
+                 (args, _, []) ->
                      Right args
-                 (_, _, errs) -> do
+                 (_, _, errs) ->
                      Left $ concat errs
 
 handleResponse :: MPD.Response (Maybe MPD.Song) -> Either MPD.MPDError (Maybe [MPD.Value])
@@ -52,14 +51,13 @@ handleResponse (Right (Just content)) = Right $ MPD.sgGetTag MPD.MUSICBRAINZ_TRA
 handleResponse (Right Nothing) = Right Nothing
 handleResponse (Left e) = Left e
 
-configure :: Config -> [(Config -> Config)] -> Config
-configure cfg (x:xs) = configure (x cfg) xs
-configure cfg [] = cfg
+configure :: Config -> [Config -> Config] -> Config
+configure = foldl (\cfg x -> x cfg)
 
 main :: IO ()
 main = do
         args <- getArgs
-        let parsedArgs = handleArgs $ parseArgs args -- parsedArgs ist Either [Char] [(Config -> Config)]
-        let config = either (\_ -> defaultConfig) (configure defaultConfig) parsedArgs --
+        let parsedArgs = handleArgs $ parseArgs args
+        let config = either (const defaultConfig) (configure defaultConfig) parsedArgs
         print config
         mpd MPD.currentSong config >>= print . handleResponse
