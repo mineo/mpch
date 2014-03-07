f0df950356105ed26c777347d932373750c0a858
Wieland Hoffmann <themineo@gmail.com>
2014-03-07 21:23:33 +0100

    moved getmbidfromresponse into handleresponse

--- mpd.hs
+++ mpd.hs
@@ -11,12 +11,10 @@ port = Nothing
 mpd :: MPD.MPD a -> IO (MPD.Response a)
 mpd = MPD.withMPD_ addr port
 
-handleResponse :: MPD.Response (Maybe MPD.Song) -> IO ()
-handleResponse (Left e) = print e
-handleResponse (Right content) = print $ fmap getMBIDfromResponse content
-
-getMBIDfromResponse :: MPD.Song -> Maybe [MPD.Value]
-getMBIDfromResponse = MPD.sgGetTag MPD.MUSICBRAINZ_TRACKID
+handleResponse :: MPD.Response (Maybe MPD.Song) -> Either MPD.MPDError (Maybe [MPD.Value])
+handleResponse (Right (Just content)) = Right $ MPD.sgGetTag MPD.MUSICBRAINZ_TRACKID content
+handleResponse (Right Nothing) = Right Nothing
+handleResponse (Left e) = Left e
 
 main :: IO ()
-main = mpd MPD.currentSong >>= handleResponse
+main = mpd MPD.currentSong >>= print . handleResponse
