553798d24af41dfc689970723194bc09617ef86c
Wieland Hoffmann <themineo@gmail.com>
2014-04-17 11:44:04 +0200

    add 'statusWrapper' for functions changing the status

--- MPCH/Commands.hs
+++ MPCH/Commands.hs
@@ -52,35 +52,43 @@ status config _ = do
               cs <- currentSong config []
               return $ showSt ++ "\n" ++  cs
 
-toggle :: CommandFunc
-toggle config _ = mpd config MPD.status >>= either (return . show) (doToggle . MPD.stState)
-    where doToggle MPD.Playing = mpd config (MPD.pause True) >> st
-          doToggle _ = mpd config (MPD.play Nothing) >> st
-          st = status config []
 
-stToggleWrapper :: (MPD.Status -> Bool) -> (Bool -> MPD.MPD a) -> CommandFunc
+-- | Executes the first argument and, if that was successful, 'status'.
+statusWrapper :: (Config -> [String] -> IO (MPD.Response a)) -> CommandFunc
+statusWrapper f config args = f config args >>= either (return . show) (\_ -> status config [])
+
+stToggleWrapper :: (MPD.Status -> Bool) -> (Bool -> MPD.MPD a) -> Config -> [String] -> IO (MPD.Response a)
 stToggleWrapper bfun mpdfun config args = case arg of
         Nothing -> execToggle
-        Just x -> mpd config (mpdfun x) >> st
+        Just x -> mpd config (mpdfun x)
     where arg = case args of
             (x:_) -> stringToBool x
             _ -> Nothing
-          st = status config []
-          execToggle = mpd config MPD.status >>= either (return . show) (doToggle . bfun)
-          doToggle True = mpd config (mpdfun False) >> st
-          doToggle False = mpd config (mpdfun True) >> st
-
-single :: CommandFunc
-single = stToggleWrapper MPD.stSingle MPD.single
+          execToggle = do
+            resp <- mpd config MPD.status
+            either (return . Left) (doToggle . bfun) resp
+          doToggle True = mpd config (mpdfun False)
+          doToggle False = mpd config (mpdfun True)
 
 consume :: CommandFunc
-consume = stToggleWrapper MPD.stConsume MPD.consume
-
-random :: CommandFunc
-random = stToggleWrapper MPD.stRandom MPD.random
+consume = statusWrapper $ stToggleWrapper MPD.stConsume MPD.consume
 
 mpdrepeat :: CommandFunc
-mpdrepeat = stToggleWrapper MPD.stRepeat MPD.repeat
+mpdrepeat = statusWrapper $ stToggleWrapper MPD.stRepeat MPD.repeat
+
+random :: CommandFunc
+random = statusWrapper $ stToggleWrapper MPD.stRandom MPD.random
+
+single :: CommandFunc
+single = statusWrapper $ stToggleWrapper MPD.stSingle MPD.single
+
+toggleImpl :: Config -> [String] -> IO (MPD.Response ())
+toggleImpl config _ = mpd config MPD.status >>= either (return . Left) (doToggle . MPD.stState)
+    where doToggle MPD.Playing = mpd config (MPD.pause True)
+          doToggle _ = mpd config (MPD.play Nothing)
+
+toggle :: CommandFunc
+toggle = statusWrapper toggleImpl
 
 tags :: [MPD.Metadata]
 tags = [MPD.MUSICBRAINZ_TRACKID, MPD.Artist, MPD.Album, MPD.Title]
